fcd9a999c7b1cc39069832b88c6c4919261b78a1
Luke Oliff <luke@lukeoliff.com>
2020-04-23 07:43:10 +0100

    change eslint format :)

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const { createEventAdapter } = require('@slack/events-api');
-const { WebClient } = require('@slack/web-api');
-const web = new WebClient(process.env.SLACK_TOKEN);
-const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
-const port = process.env.PORT || 3000;
+const { createEventAdapter } = require('@slack/events-api')
+const { WebClient } = require('@slack/web-api')
+const web = new WebClient(process.env.SLACK_TOKEN)
+const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET)
+const port = process.env.PORT || 3000
 
 slackEvents.on('message', (event) => {
   if (
-    event.hasOwnProperty('text') &&
+    Object.prototype.hasOwnProperty.call(event, 'text') &&
     event.text.toLowerCase().includes('guys')
   ) {
     (async () => {
@@ -17,22 +17,22 @@ slackEvents.on('message', (event) => {
         'Please bear in mind that the makeup of this Slack is ' +
         'very diverse, and some people feel excluded by the ' +
         'use of the term “guys”. Maybe you could try using ' +
-        '_people_, _team_, _all_, _folks_, _everyone_, or _y\'all_?';
+        '_people_, _team_, _all_, _folks_, _everyone_, or _y\'all_?'
       const infoLink =
         process.env.GUYS_INFO_LINK ||
-        'https://dev.to/kmelve/the-problem-with-you-guys-51h7';
+        'https://dev.to/kmelve/the-problem-with-you-guys-51h7'
 
       await web.chat.postEphemeral({
         channel: event.channel,
         user: event.user,
-        text: messageBody + ` (<${infoLink}|What\'s this then?>).`,
-      });
-    })();
+        text: messageBody + ` (<${infoLink}|What\'s this then?>).`
+      })
+    })()
   }
-});
+})
 
-slackEvents.on('error', console.error);
+slackEvents.on('error', console.error)
 
 slackEvents.start(port).then(() => {
-  console.log(`server listening on port ${port}`);
-});
+  console.log(`server listening on port ${port}`)
+})
