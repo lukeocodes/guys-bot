c428f8b79a45bff8d70ec59bed39fb3884e40308
Andrew Stanbury <github@andrewstanbury.com>
2020-09-19 14:02:32 -0700

    Private message scraping

--- packages/client/src/controller.ts
+++ packages/client/src/controller.ts
@@ -9,39 +9,47 @@ export class Controller {
     const web = new WebClient(state.accessToken)
     const slackEvents = createEventAdapter(state.signingSecret)
     const port = process.env.PORT || 3000
-    
+
     slackEvents.on('message', (event) => {
-      if (
-        Object.prototype.hasOwnProperty.call(event, 'text') &&
-        event.text.toLowerCase().includes('guys')
-      ) {
-        (async () => {
-          try {
-            const messageBody =
-            process.env.GUYS_MESSAGE ||
-            'Please bear in mind that the makeup of this Slack is ' +
-            'very diverse, and some people feel excluded by the ' +
-            'use of the term “guys”. Maybe you could try using ' +
-            '_people_, _team_, _all_, _folks_, _everyone_, or _y\'all_?'
-          const infoLink =
-            process.env.GUYS_INFO_LINK ||
-            'https://dev.to/kmelve/the-problem-with-you-guys-51h7'
-    
-          await web.chat.postEphemeral({
-            channel: event.channel,
-            user: event.user,
-            text: messageBody + ` (<${infoLink}|What\'s this then?>).`
-          })
+      if (Object.prototype.hasOwnProperty.call(event, 'channel_type')) {
+        if (event.channel_type == 'channel') {
+          if (
+            Object.prototype.hasOwnProperty.call(event, 'text') &&
+            event.text.toLowerCase().includes('guys')
+          ) {
+            web.chat.postEphemeral({
+              channel: event.channel,
+              user: event.user,
+              text: 'Please bear in mind that the makeup of this Slack is very diverse, and some people feel excluded by the use of the term “guys”. Maybe you could try using _people_, _team_, _all_, _folks_, _everyone_, or _y\'all_?'
+            })
           }
-          catch(err) {
-            console.log(err.message);
+        }
+
+        if (event.channel_type == 'im') {
+          if (
+            Object.prototype.hasOwnProperty.call(event, 'text') &&
+            event.text.toLowerCase().includes('guys')
+          ) {
+            web.chat.postEphemeral({
+              channel: event.user,
+              user: event.user,
+              text: 'Please bear in mind that the makeup of this Slack is very diverse, and some people feel excluded by the use of the term “guys”. Maybe you could try using _people_, _team_, _all_, _folks_, _everyone_, or _y\'all_?'
+            })
           }
-        })()
+        }
+
+        if (event.channel_type == 'mpim') {
+          console.log('message.mpim');
+        }
+
+        if (event.channel_type == 'groups') {
+          console.log('message.groups');
+        }
       }
     })
-    
+
     slackEvents.on('error', console.error)
-    
+
     slackEvents.start(port).then(() => {
       console.log(`server listening on port ${port}`)
     })
