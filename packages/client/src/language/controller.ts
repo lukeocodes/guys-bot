aa3dabef135eea8022fcd54a37a72067f8ed83ef
Danielle Brook-Roberge <danielle@brook-roberge.ca>
2020-12-11 14:33:02 -0800

    Make matching more precise

--- packages/client/src/language/controller.ts
+++ packages/client/src/language/controller.ts
@@ -1,58 +1,42 @@
 import { SlackApi } from 'slack-api';
-import { Language } from '.'
-import { StateStream } from 'state-stream'
-import * as config from "./state.json"
+import { Language } from '.';
+import { StateStream } from 'state-stream';
+import * as config from './state.json';
+
+const VALID_CHANNEL_TYPES = ['channel', 'group', 'mpim', 'im'];
 
 export class Controller {
-    public slackBlockKit: SlackApi.BlockKit.Block.Controller
-    public state: Language.State
+    public slackBlockKit: SlackApi.BlockKit.Block.Controller;
+    public state: Language.State;
 
     constructor() {
-        this.slackBlockKit = new SlackApi.BlockKit.Block.Controller()
+        this.slackBlockKit = new SlackApi.BlockKit.Block.Controller();
     }
 
     setState(state: Language.State) {
-        this.state = StateStream.Merge(state, config)
+        this.state = StateStream.Merge(state, config);
     }
 
     public message() {
         return {
             trigger: 'message',
             callback: (view, client, body) => {
-                if (
-                    view.channel_type == 'channel' ||
-                    view.channel_type == 'group' ||
-                    view.channel_type == 'mpim' ||
-                    view.channel_type == 'im'
-                ) {
-                    this.state.terms.forEach(term => {
-                        var viewText = view.text.toLowerCase()
-                        var termText = term.text.toLowerCase()
+                if (VALID_CHANNEL_TYPES.includes(view.channel_type)) {
+                    const viewText = view.text.toLowerCase();
+                    this.state.terms.forEach((term) => {
+                        const termRegexp = new RegExp(`\b${term.text}\b`, 'i');
 
-                        if (
-                            viewText == termText ||
-                            viewText.includes(' ' + termText + ' ') ||
-                            viewText.includes(' ' + termText + '.') ||
-                            viewText.includes(' ' + termText + '?') ||
-                            viewText.includes(' ' + termText + '!') ||
-                            viewText.includes(' ' + termText + ',') ||
-                            viewText.startsWith(termText + ' ') ||
-                            viewText.startsWith(termText + '.') ||
-                            viewText.startsWith(termText + '?') ||
-                            viewText.startsWith(termText + '!') ||
-                            viewText.startsWith(termText + ',') ||
-                            viewText.endsWith(' ' + termText)
-                        ) {
+                        if (termRegexp.test(viewText)) {
                             client.chat.postEphemeral({
                                 token: client.token,
                                 channel: view.user,
                                 user: view.user,
-                                text: term.text + ": " + term.response
-                            })
+                                text: term.text + ': ' + term.response,
+                            });
                         }
-                    })
+                    });
                 }
-            }
-        }
+            },
+        };
     }
 }
